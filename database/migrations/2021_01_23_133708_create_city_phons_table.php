97aa11fea78e5cfe0b7a8c6b3eaa5b70ce85ea04
Aram Harutyunyan <aram30041992@gmail.com>
2021-01-23 17:44:43 +0400

    some migrations

---
+++ database/migrations/2021_01_23_133708_create_city_phons_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCityPhonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('city_phons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('phone');
+            $table->unsignedInteger('city_id');
+            $table->foreign('city_id')
+                ->references('id')
+                ->on('cities');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('city_phons');
+    }
+}
