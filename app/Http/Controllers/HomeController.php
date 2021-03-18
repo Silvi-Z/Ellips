c5059a5a4ca99838af0570a3e40b260a9bc928e7
david-aramyan <davidaramyan55@gmail.com>
2021-03-18 12:01:25 +0400

    bug fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -91,7 +91,7 @@ class HomeController extends Controller
 
         $products = Product::whereHas('categories',function ($query) use($category){
             $query->where('category_id',$category->id);
-        })->search($request)->paginate(9);
+        })->search($request)->paginate(12);
         $products->appends($request->all());
         return view('productPage')->with([
             'category'=>$category,
